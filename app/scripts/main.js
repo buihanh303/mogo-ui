14e254d2fb3efc00074bd15de0bc4d833987b4ef
buihanh <buihanh303@gmail.com>
2021-11-11 14:46:31 +0700

    complete project

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,3 @@
-// Slick slider
-// $(document).ready(function () {
-//   $(".quote-list").slick({
-//     prevArrow:
-//       "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
-//     nextArrow:
-//       "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
-//     responsive: [
-//       {
-//         breakpoint: 767,
-//         settings: {
-//           arrows: false,
-//         },
-//       },
-//     ],
-//   });
-// });
-
 const btnToggle = document.querySelector(".header-toggle");
 const menuHeader = document.querySelector(".header-menu");
 const expandClass = "is-expand";
@@ -28,4 +10,23 @@ window.addEventListener("click", (e) => {
   if (!menuHeader.contains(e.target) && !e.target.matches(".header-toggle")) {
     menuHeader.classList.remove(expandClass);
   }
+});
+
+// slider
+
+$(document).ready(function () {
+  $(".quote-container").slick({
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 1023,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
 });
